dfaa282f2acd88222d6fbf9c7b91c0ec18a3fef2
Zuozhi Wang <zuozhiw@gmail.com>
2021-01-14 01:46:05 -0800

    suport multi input operators

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/operators/SinkOpExecConfig.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/operators/SinkOpExecConfig.scala
@@ -3,5 +3,4 @@ package edu.uci.ics.amber.engine.operators
 import edu.uci.ics.amber.engine.common.ambertag.OperatorIdentifier
 
 abstract class SinkOpExecConfig(tag: OperatorIdentifier) extends OpExecConfig(tag) {
-  override def getInputNum(from: OperatorIdentifier): Int = 0
 }
