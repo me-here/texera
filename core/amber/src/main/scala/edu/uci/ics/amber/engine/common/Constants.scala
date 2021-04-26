dacd7bd52dcde329e32c4737c5424b3b3368c0d2
Avinash Kumar <avinash0161@yahoo.com>
2021-04-26 13:43:49 -0700

    increasing range max

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
@@ -24,7 +24,7 @@ object Constants {
   // sort-skew research related
   val sortingFrequency: Int = 1000
   val lowerLimit: Float = 0f
-  val upperLimit: Float = 350000f
+  val upperLimit: Float = 500000f
   val sortExperiment: Boolean = false
 
   type joinType = String
