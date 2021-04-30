bb5e2c05542f5d68dd92873bb8937447e8fbc847
Avinash Kumar <avinash0161@yahoo.com>
2021-04-30 15:57:15 -0700

    adding scripts

--- core/scripts/server.sh
+++ core/scripts/server.sh
@@ -1,2 +1,4 @@
 cd amber
-sbt "runMain edu.uci.ics.texera.web.TexeraWebApplication" 
+export SBT_OPTS="-Xmx31G -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -Xss2M  -Duser.timezone=GMT"
+sudo nohup sbt -mem 31744 "runMain edu.uci.ics.texera.web.TexeraWebApplication false" > ~/server.log 2>&1 &
+echo "success"
