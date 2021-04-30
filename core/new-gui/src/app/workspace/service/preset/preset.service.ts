f0d41aead7f769f75a02b6800d6e02161806c10a
Albert Liu <ayliu5@uci.edu>
2021-04-29 21:21:32 -0700

    refactor & bugfix preset service

--- core/new-gui/src/app/workspace/service/preset/preset.service.ts
+++ core/new-gui/src/app/workspace/service/preset/preset.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@angular/core';
 import * as Ajv from 'ajv';
-import { JSONSchema7 } from 'json-schema';
-import { cloneDeep, isEqual, merge } from 'lodash';
+import { cloneDeep, has, isEqual, merge, pickBy } from 'lodash';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { Observable, Subject } from 'rxjs';
 import { DictionaryService, JSONValue, UserDictionary } from 'src/app/common/service/user/user-dictionary/dictionary.service';
-import { OperatorPredicate } from '../../types/workflow-common.interface';
-import { DynamicSchemaService } from '../dynamic-schema/dynamic-schema.service';
+import { isType } from 'src/app/common/util/assert';
+import { CustomJSONSchema7 } from '../../types/custom-json-schema.interface';
 import { OperatorMetadataService } from '../operator-metadata/operator-metadata.service';
 import { WorkflowActionService } from '../workflow-graph/model/workflow-action.service';
 
@@ -19,6 +18,7 @@ export type Preset = {[key: string]: string|number|boolean};
 })
 export class PresetService {
   private static DICT_PREFIX = 'Preset';
+  private static ajvStrip = new Ajv({ useDefaults: true, removeAdditional: true});
 
   public readonly applyPresetStream: Observable<{type: string, target: string, preset: Preset}>;
   public readonly savePresetsStream: Observable<{type: string, target: string, presets: Preset[]}>;
@@ -49,14 +49,17 @@ export class PresetService {
   public savePresets(type: string, target: string, presets: Preset[],
     displayMessage?: string|null, messageType: AlertMessageType = 'success') {
 
-    this.presetDict[`${type}-${target}`] = presets;
+    if (presets.length > 0) {
+      this.presetDict[`${type}-${target}`] = presets;
+    } else {
+      delete this.presetDict[`${type}-${target}`];
+    }
     this.savePresetSubject.next({type: type, target: target, presets: presets});
     this.displaySavePresetMessage(messageType, displayMessage);
   }
 
   public getPresets(type: string, target: string): Readonly<Preset[]> {
     const presets = this.presetDict[`${type}-${target}`] ?? [];
-    console.log(presets);
     if (this.isPresetArray(presets)) {
       return presets;
     } else {
@@ -70,24 +73,23 @@ export class PresetService {
         this.workflowActionService.getTexeraGraph().getOperator(operatorID).operatorType).jsonSchema);
     const fitsSchema = this.ajv.compile(presetSchema)(preset);
     const noEmptyProperties = Object.keys(preset).every(
-      (key: string) => typeof preset !== 'string' || ((<string>preset[key]).trim()).length > 0);
+      (key: string) => !isType(preset[key], 'string') || ((<string> preset[key]).trim()).length > 0);
 
     return fitsSchema && noEmptyProperties;
   }
 
   public isValidNewOperatorPreset(preset: Preset, operatorID: string): boolean {
-    const existsAlready = this.getPresets('operator', this.workflowActionService.getTexeraGraph().getOperator(operatorID).operatorType)
+    const isNewPreset = !this.getPresets('operator', this.workflowActionService.getTexeraGraph().getOperator(operatorID).operatorType)
       .some(existingPreset => isEqual(preset, existingPreset));
 
-    return this.isValidOperatorPreset(preset, operatorID) && !existsAlready;
-
+    return isNewPreset && this.isValidOperatorPreset(preset, operatorID);
   }
 
   private getPresetDict(): UserDictionary {
     const dict = this.dictionaryService.forceGetUserDictionary();
     return new Proxy(dict, {
-      get(target: UserDictionary, key: string) {
-        return target[`${PresetService.DICT_PREFIX}-${key}`];
+      get(target: UserDictionary, key: symbol) {
+        return target[`${PresetService.DICT_PREFIX}-${key.toString()}`];
       },
       set(target: UserDictionary, key: string, value: JSONValue) {
         target[`${PresetService.DICT_PREFIX}-${key}`] = value;
@@ -159,32 +161,52 @@ export class PresetService {
   private handleApplyOperatorPresets() {
     this.applyPresetStream.subscribe({
       next: (applyEvent) => {
-        if ( applyEvent.type === 'operator' && this.workflowActionService.getTexeraGraph().hasOperator(applyEvent.target) &&
-          this.isValidOperatorPreset(applyEvent.preset, applyEvent.target)) {
-          console.log('applypreset', applyEvent);
-          this.workflowActionService.setOperatorProperty(
-            applyEvent.target,
-            merge(this.workflowActionService.getTexeraGraph().getOperator(applyEvent.target).operatorProperties, applyEvent.preset)
-          );
+        if ( applyEvent.type === 'operator' && this.workflowActionService.getTexeraGraph().hasOperator(applyEvent.target)) {
+          if (this.isValidOperatorPreset(applyEvent.preset, applyEvent.target)) {
+            this.workflowActionService.setOperatorProperty(
+              applyEvent.target,
+              merge(cloneDeep(this.workflowActionService.getTexeraGraph().getOperator(applyEvent.target).operatorProperties),
+                applyEvent.preset));
+          } else {
+            const schema = PresetService.getOperatorPresetSchema(
+              this.operatorMetadataService.getOperatorSchema(
+                this.workflowActionService.getTexeraGraph().getOperator(applyEvent.target).operatorType).jsonSchema);
+            throw new Error(`Error applying preset: preset ${applyEvent.preset} was not a valid preset for ${applyEvent.target} with schema ${schema}`);
+          }
         }
       }
     });
   }
 
-  public static getOperatorPresetSchema(operatorSchema: JSONSchema7): JSONSchema7 {
+  public static getOperatorPresetSchema(operatorSchema: CustomJSONSchema7): CustomJSONSchema7 {
     const copy = cloneDeep(operatorSchema);
-    if (copy.properties === undefined) {
-      throw new Error(`provided operator schema ${operatorSchema} has no properties`);
-    } else {
-      copy.required = [];
-      for (const key of Object.keys(copy.properties)) {
-        if (!(copy.properties[key] as any)['enable-presets']) {
-          delete copy.properties[key];
-        } else {
-          copy.required.push(key);
-        }
-      }
+    if (operatorSchema.properties === undefined) { throw new Error(`provided operator schema ${operatorSchema} has no properties`); }
+    const properties = pickBy(copy.properties, (prop) => has(prop, 'enable-presets') && (prop as any)['enable-presets'] === true);
+    if (isEqual(properties, {})) { throw new Error(`provided operator schema ${operatorSchema} has no preset properties`); }
+    return {
+      type: 'object',
+      properties: properties,
+      required: Object.keys(properties),
+      additionalProperties: false,
+    };
+  }
+
+  public static getOperatorPreset(operatorSchema: CustomJSONSchema7, operatorProperties: object) {
+    const copy = cloneDeep(operatorProperties as Preset);
+    const presetSchema = this.getOperatorPresetSchema(operatorSchema);
+    const strip = this.ajvStrip.compile(presetSchema); // this validator also removes extra properties that aren't a part of the preset
+    if (strip(copy)) {
       return copy;
+    } else {
+      throw new Error(`provided operator properties ${operatorProperties} does not conform to preset schema ${presetSchema}`);
     }
+  }
+
+  public static filterOperatorPresetProperties(operatorSchema: CustomJSONSchema7, operatorProperties: object) {
+    const copy = cloneDeep(operatorProperties as Preset);
+    const presetSchema = this.getOperatorPresetSchema(operatorSchema);
+    const strip = this.ajvStrip.compile(presetSchema); // this validator also removes extra properties that aren't a part of the preset
+    strip(copy);
+    return copy;
   }
 }
