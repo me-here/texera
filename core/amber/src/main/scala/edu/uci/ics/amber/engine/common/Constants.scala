d63e1dbca3800e511963aacd18645c10fa853a20
Avinash Kumar <avinash0161@yahoo.com>
2021-05-22 17:19:59 -0700

    change mitigation

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
@@ -23,7 +23,7 @@ object Constants {
 
   // sort-skew research related
   val eachTransferredListSize: Int = 100000
-  val skewToFreeTransferRatio: Int = 1
+  val skewToFreeTransferRatio: Int = 2
   val lowerLimit: Float = 0f
   val upperLimit: Float = 600000f // 30gb
   val sortExperiment: Boolean = true
