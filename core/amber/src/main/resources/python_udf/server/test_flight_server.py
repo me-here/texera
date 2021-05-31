74690144ec97dc14aa23f9fc5a497a4f8260826f
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-05-31 10:12:17 -0700

    add some tests

--- core/amber/src/main/resources/python_udf/server/test_flight_server.py
+++ core/amber/src/main/resources/python_udf/server/test_flight_server.py
@@ -6,47 +6,78 @@ import pytest
 from server.flight_server import FlightServer
 from server.mock_client import UDFMockClient
 
+
+def hello():
+    return "hello-function"
+
+
+class HelloClass:
+    def __call__(self):
+        return "hello-class"
+
+
+# prepare some functions to be test for registration
 test_funcs = {
     "hello": lambda: "hello",
-    "this is another call": lambda: "ack!!!"
+    "this is another call": lambda: "ack!!!",
+    "hello-function": hello,
+    "hello-class": HelloClass()
 }
 
 
-@pytest.fixture()
-def launch_server(args):
-    p1 = Process(target=server, args=args)
-    p1.start()
-    time.sleep(1)
-    yield
-    p1.kill()
+class TestSeverIntegration:
 
+    @pytest.fixture
+    def launch_server(self, args):
+        p1 = Process(target=self.start_flight_server, args=args)
+        p1.start()
+        time.sleep(0.5)
+        yield
+        p1.kill()
 
-def server(*funcs):
-    host: str = "localhost"
-    port: int = 5005
-    scheme: str = "grpc+tcp"
-    location = "{}://{}:{}".format(scheme, host, port)
+    @staticmethod
+    def start_flight_server(*func_names):
+        host: str = "localhost"
+        port: int = 5005
+        scheme: str = "grpc+tcp"
+        location = f"{scheme}://{host}:{port}"
+        server = FlightServer(host, location)
+        for name in func_names:
+            server.register(name, test_funcs[name])
+        server.serve()
 
-    server = FlightServer(host, location)
-    for name in funcs:
-        server.register(name, test_funcs[name])
-    server.serve()
+    @pytest.mark.parametrize('args', [tuple()])
+    def test_server_can_start(self, launch_server):
+        client = UDFMockClient()
+        # should by default only have shutdown
+        assert len(client.list_actions()) == 1
+        action = client.list_actions()[0]
+        assert action.type == "shutdown"
+        assert action.description == "Shut down this server."
 
+    @pytest.mark.parametrize('args', [tuple()])
+    def test_server_can_shutdown_by_client(self, launch_server):
+        client = UDFMockClient()
+        assert client.call("shutdown") == b''
 
-@pytest.mark.parametrize('args', [tuple()])
-def test_server_can_start(launch_server):
-    client = UDFMockClient()
-    # should by default only have shutdown
-    assert len(client.list_actions()) == 1
-    action = client.list_actions()[0]
-    assert action.type == "shutdown"
-    assert action.description == "Shut down this server."
+    @pytest.mark.parametrize('args', [("hello", "this is another call")])
+    def test_server_call_registered_lambdas(self, launch_server):
+        client = UDFMockClient()
+        assert len(client.list_actions()) == 3
+        assert client.call("hello") == b'hello'
+        assert client.call("this is another call") == b'ack!!!'
+        assert client.call("shutdown") == b''
 
+    @pytest.mark.parametrize('args', [("hello-function",)])
+    def test_server_call_registered_function(self, launch_server):
+        client = UDFMockClient()
+        assert len(client.list_actions()) == 2
+        assert client.call("hello-function") == b'hello-function'
+        assert client.call("shutdown") == b''
 
-@pytest.mark.parametrize('args', [("hello", "this is another call")])
-def test_basic_server_client(launch_server):
-    client = UDFMockClient()
-    assert len(client.list_actions()) == 3
-    assert client.call("hello") == b'hello'
-    assert client.call("this is another call") == b'ack!!!'
-    assert client.call("shutdown") == b''
+    @pytest.mark.parametrize('args', [("hello-class",)])
+    def test_server_call_registered_callable_class(self, launch_server):
+        client = UDFMockClient()
+        assert len(client.list_actions()) == 2
+        assert client.call("hello-class") == b'hello-class'
+        assert client.call("shutdown") == b''
