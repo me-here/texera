66db3034eb8fd79924c87b8ac8abe9e8f7cea521
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-06-07 00:24:11 -0700

    change name to sequence

--- core/amber/src/main/python/worker/util/stable_priority_queue.py
+++ core/amber/src/main/python/worker/util/stable_priority_queue.py
@@ -26,21 +26,21 @@ class QueueControl(QueueElement):
 
 
 class StablePriorityQueue(PriorityQueue):
-    counter = 0
+    sequence = 0
 
     def put(self, item: QueueElement, block: bool = ..., timeout: Optional[float] = ...) -> None:
         # OPTIMIZE:
         #    The standard implementation of queue.PriorityQueue is not stable.
         #    Here is a temporary solution to make sure the queue is stable.
         #    However, the current implementation has the following two issues:
-        #       1. violating thread-safe, the counter change may not be atomic.from
-        #       2. the counter keeps increment, which will have a performance issue when the number gets
-        #          really big. Python handles infinite int, but it gets really slow after 2**10000000
+        #       1. violating thread-safe, the sequence change may not be atomic.
+        #       2. the sequence keeps increment, which will have a performance issue when the number gets
+        #          really big. Python handles infinite int, but it gets really slow after 2**10000000.
         #
-        self.counter += 1
+        self.sequence += 1
         if not isinstance(item, QueueElement):
             raise TypeError(f"Unsupported type {type(item)} to put in the {self.__class__.__name__}.")
-        super(StablePriorityQueue, self).put(PrioritizedItem((item.priority, self.counter), item), block, timeout)
+        super(StablePriorityQueue, self).put(PrioritizedItem((item.priority, self.sequence), item), block, timeout)
 
     def get(self, block: bool = ..., timeout: Optional[float] = ...) -> Any:
         prioritized_item: PrioritizedItem = super(StablePriorityQueue, self).get()
