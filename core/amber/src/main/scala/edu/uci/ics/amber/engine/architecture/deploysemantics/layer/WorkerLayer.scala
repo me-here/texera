effa82c55f6ed4a20714c88608f80a4afdaefae8
Yicong Huang <17627829+Yicong-Huang@users.noreply.github.com>
2021-08-02 22:55:03 -0700

    make it more readable

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/deploysemantics/layer/WorkerLayer.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/deploysemantics/layer/WorkerLayer.scala
@@ -22,7 +22,7 @@ import scala.collection.mutable
 
 class WorkerLayer(
     val id: LayerIdentity,
-    var metadata: Int => IOperatorExecutor,
+    var initIOperatorExecutor: Int => IOperatorExecutor,
     var numWorkers: Int,
     val deploymentFilter: DeploymentFilter,
     val deployStrategy: DeployStrategy
@@ -51,6 +51,9 @@ class WorkerLayer(
 
   def statistics: Array[WorkerStatistics] = workers.values.map(_.stats).toArray
 
+  def workerToOperatorExec: mutable.HashMap[ActorVirtualIdentity, IOperatorExecutor] =
+    mutable.HashMap()
+
   def build(
       prev: Array[(OpExecConfig, WorkerLayer)],
       all: Array[Address],
@@ -60,24 +63,24 @@ class WorkerLayer(
   ): Unit = {
     deployStrategy.initialize(deploymentFilter.filter(prev, all, context.self.path.address))
     workers = (0 until numWorkers).map { i =>
-      val m = metadata(i)
-      val workerID = ActorVirtualIdentity(s"Worker-$id-[$i]")
-      val d = deployStrategy.next()
-      val ref = context.actorOf(
-        if (m.isInstanceOf[PythonUDFOpExecV2]) {
+      val operatorExecutor: IOperatorExecutor = initIOperatorExecutor(i)
+      val workerId: ActorVirtualIdentity = ActorVirtualIdentity(s"Worker-$id-[$i]")
+      val address: Address = deployStrategy.next()
+      val ref: ActorRef = context.actorOf(
+        if (operatorExecutor.isInstanceOf[PythonUDFOpExecV2]) {
           PythonWorkflowWorker
-            .props(workerID, m, parentNetworkCommunicationActorRef)
-            .withDeploy(Deploy(scope = RemoteScope(d)))
+            .props(workerId, operatorExecutor, parentNetworkCommunicationActorRef)
+            .withDeploy(Deploy(scope = RemoteScope(address)))
         } else {
           WorkflowWorker
-            .props(workerID, m, parentNetworkCommunicationActorRef)
-            .withDeploy(Deploy(scope = RemoteScope(d)))
+            .props(workerId, operatorExecutor, parentNetworkCommunicationActorRef)
+            .withDeploy(Deploy(scope = RemoteScope(address)))
         }
       )
-      parentNetworkCommunicationActorRef ! RegisterActorRef(workerID, ref)
-      workerToLayer(workerID) = this
-      workerID -> WorkerInfo(
-        workerID,
+      parentNetworkCommunicationActorRef ! RegisterActorRef(workerId, ref)
+      workerToLayer(workerId) = this
+      workerId -> WorkerInfo(
+        workerId,
         UNINITIALIZED,
         WorkerStatistics(UNINITIALIZED, 0, 0)
       )
