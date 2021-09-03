442340db8988e5e9b95e9c4843653a4d02af99ee
DanielMao1 <daniel4006695539@gmail.com>
2021-09-04 00:25:10 +0800

    reformatCode

--- core/amber/src/main/scala/edu/uci/ics/texera/web/TexeraRunWorker.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/TexeraRunWorker.scala
@@ -8,9 +8,6 @@ object TexeraRunWorker {
 
   def main(args: Array[String]): Unit = {
     Kamon.init()
-    Kamon.reconfigure(
-      ConfigFactory.parseString("kamon.metric.tick-interval=10s").withFallback(Kamon.config())
-    )
     // start actor system worker node
     AmberUtils.startActorWorker(Option.empty)
   }
