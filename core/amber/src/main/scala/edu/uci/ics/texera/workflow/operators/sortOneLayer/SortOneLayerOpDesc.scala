3f0ad36b206f12384ff90fa8f9273c53ee8be664
Avinash Kumar <avinash0161@yahoo.com>
2021-10-17 23:34:32 -0700

    mini-partitions

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/sortOneLayer/SortOneLayerOpDesc.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/sortOneLayer/SortOneLayerOpDesc.scala
@@ -25,7 +25,7 @@ class SortOneLayerOpDesc extends OperatorDescriptor {
     opExecConfig = new SortOneLayerOpExecConfig(
       this.operatorIdentifier,
       sortAttributeName,
-      Constants.defaultNumWorkers
+      100
     )
     opExecConfig
   }
