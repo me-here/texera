a9b3e72ec21e482929221fb23c090f10bec02039
Avinash Kumar <avinash0161@yahoo.com>
2021-11-06 18:16:25 -0700

    sort exp

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
@@ -27,7 +27,7 @@ object Constants {
   val eachTransferredListSize: Int = 100000
   val lowerLimit: Float = 0f
   val upperLimit: Float = 600000f // 30gb
-  val sortExperiment: Boolean = false
+  val sortExperiment: Boolean = true
 
   val singleIterationOnly: Boolean = true
   val freeSkewedThreshold: Int = 100000
