f7be6659ad5fb6a768c32a2ad5d7c9cc5c0144dd
Avinash Kumar <avinash0161@yahoo.com>
2021-11-07 19:22:49 -0800

    sort exp

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
@@ -28,7 +28,7 @@ object Constants {
 
   val sortExperiment: Boolean = true
   val onlyDetectSkew: Boolean = false
-  var threshold: Int = 2500
+  var threshold: Int = 10
   var freeSkewedThreshold: Int = 100
   val firstPhaseNum = 1
   val firstPhaseDen = 1
