9f611dfe23f46e9925528126a7076a0f597fc488
Shengquan Ni <121989255@qq.com>
2021-11-26 13:21:05 -0800

    unmitigated

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
@@ -27,7 +27,7 @@ object Constants {
   val upperLimit: Float = 600000f // 30gb
 
   val sortExperiment: Boolean = false
-  val onlyDetectSkew: Boolean = false
+  val onlyDetectSkew: Boolean = true
   var threshold: Int = 100
   var freeSkewedThreshold: Int = 3000 // 300
   val firstPhaseNum = 1 // 9
