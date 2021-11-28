933eb5dcc91f0ab63f2969c8b77865d123b338ca
Avinash Kumar <avinash0161@yahoo.com>
2021-11-27 16:46:20 -0800

    benefit

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
@@ -30,8 +30,8 @@ object Constants {
   val onlyDetectSkew: Boolean = false
   var threshold: Int = 10
   var freeSkewedThreshold: Int = 30000 // 300
-  val firstPhaseNum = 9 // 9
-  val firstPhaseDen = 10 // 10
+  val firstPhaseNum = 1 // 9
+  val firstPhaseDen = 1 // 10
 
   val singleIterationOnly: Boolean = true // set freeSkewedThreshold to large number
   val dynamicThreshold: Boolean = false
