33cc68b0bb16afa9b25c8fb02f2dce80677e33f2
Avinash Kumar <avinash0161@yahoo.com>
2021-12-03 19:28:45 -0800

    fixing bug

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
@@ -29,11 +29,11 @@ object Constants {
   val sortExperiment: Boolean = false
   val onlyDetectSkew: Boolean = true
   var threshold: Int = 100
-  var freeSkewedThreshold: Int = 300 // 300
+  var freeSkewedThreshold: Int = 3000 // 300
   val firstPhaseNum = 1 // 9
   val firstPhaseDen = 2 // 10
 
-  val singleIterationOnly: Boolean = false // set freeSkewedThreshold to large number
+  val singleIterationOnly: Boolean = true // set freeSkewedThreshold to large number
   val dynamicThreshold: Boolean = false // set freeSkewedThreshold to be equal to threshold
   val dynamicDistributionExp: Boolean = false
   val controllerHistoryLimitPerWorker: Int = 10000
