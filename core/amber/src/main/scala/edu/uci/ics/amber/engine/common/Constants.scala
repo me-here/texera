4b2fe457f91953f7a53979e7716cae27d61f44a1
Shengquan Ni <121989255@qq.com>
2021-12-04 23:43:51 -0800

    change distribution exp - flowjoin

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
@@ -29,11 +29,11 @@ object Constants {
   val sortExperiment: Boolean = false
   val onlyDetectSkew: Boolean = false
   var threshold: Int = 100
-  var freeSkewedThreshold: Int = 300 // 300
-  val firstPhaseNum = 9 // 9
-  val firstPhaseDen = 10 // 10
+  var freeSkewedThreshold: Int = 300000000 // 300
+  val firstPhaseNum = 1 // 9
+  val firstPhaseDen = 2 // 10
 
-  val singleIterationOnly: Boolean = false // set freeSkewedThreshold to large number
+  val singleIterationOnly: Boolean = true // set freeSkewedThreshold to large number
   val dynamicThreshold: Boolean = false // set freeSkewedThreshold to be equal to threshold
   val dynamicDistributionExp: Boolean = true
   val dynamicDistributionFluxExp: Boolean = false
