08933893a3fbc74221106ca5d7885ca144f7d084
Avinash Kumar <avinash0161@yahoo.com>
2021-12-04 17:35:50 -0800

    flowjoin

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
@@ -17,7 +17,7 @@ object Constants {
   // join-skew reserach related
   val gcpExp: Boolean = true
   val samplingResetFrequency: Int = 2000
-  val startDetection: FiniteDuration = 11000.milliseconds // 100.milliseconds
+  val startDetection: FiniteDuration = 15000.milliseconds // 100.milliseconds
   val detectionPeriod: FiniteDuration = 2.seconds
   val printResultsInConsole: Boolean = true
 
