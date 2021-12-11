ac6594fc3b51d4109b65c13a96f6c5500a6ad95f
Avinash Kumar <avinash0161@yahoo.com>
2021-12-11 14:17:01 -0800

    changes

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/hashJoinTweets/HashJoinTweetsOpExec.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/hashJoinTweets/HashJoinTweetsOpExec.scala
@@ -70,7 +70,7 @@ class HashJoinTweetsOpExec[K](
               val tweetText = t.getField(tweetTextAttr).asInstanceOf[String]
               var countPresent: Int = 0
               val slangs: Array[String] = Array("there", "hella", "gaper", "bomb", "poho", "potato", "dunks")
-              for (i <- 0 to 2) {
+              for (i <- 0 to 3) {
                 slangs.foreach(slang => {
                   if (tweetText.toLowerCase().contains(slang.toLowerCase())) {
                     countPresent += 1
@@ -86,7 +86,7 @@ class HashJoinTweetsOpExec[K](
               var countPresent: Int = 0
               if (Constants.dynamicThreshold) {
                 var slangs: Array[String] = Array("there", "hella", "gaper", "bomb", "poho", "potato", "dunks")
-                for (i <- 0 to 2) {
+                for (i <- 0 to 3) {
                   slangs.foreach(slang => {
                     if (tweetText.toLowerCase().contains(slang.toLowerCase())) {
                       countPresent += 1
@@ -95,7 +95,7 @@ class HashJoinTweetsOpExec[K](
                 }
               } else {
                 var slangs: Array[String] = Array("there", "hella", "gaper", "bomb", "poho", "potato", "dunks")
-                for (i <- 0 to 2) {
+                for (i <- 0 to 3) {
                   slangs.foreach(slang => {
                     if (tweetText.toLowerCase().contains(slang.toLowerCase())) {
                       countPresent += 1
