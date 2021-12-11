a6b54ea64e4f3a5e5acf02a4eeb4f15728ed3edf
Avinash Kumar <avinash0161@yahoo.com>
2021-12-10 22:58:42 -0800

    changes

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
@@ -27,7 +27,7 @@ object Constants {
   val upperLimit: Float = 600000f // 30gb
 
   val sortExperiment: Boolean = false
-  val onlyDetectSkew: Boolean = true
+  val onlyDetectSkew: Boolean = false
   var threshold: Int = 10
   var freeSkewedThreshold: Int = 10 // 300
   var firstphaseThresholdWhenRollingBack: Int = 100
