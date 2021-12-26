a0b38df45f26addbebec2cbbdc41f3f10034b657
Avinash Kumar <avinash0161@yahoo.com>
2021-12-25 21:53:04 -0800

    sort

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/common/Constants.scala
@@ -28,11 +28,11 @@ object Constants {
 
   val sortExperiment: Boolean = false
   val onlyDetectSkew: Boolean = false
-  var threshold: Int = 1800
-  var freeSkewedThreshold: Int = 1800 // 300
+  var threshold: Int = 100
+  var freeSkewedThreshold: Int = 300 // 300
   var firstphaseThresholdWhenRollingBack: Int = 100
-  val firstPhaseNum = 3 // 9
-  val firstPhaseDen = 4 // 10
+  val firstPhaseNum = 9 // 9
+  val firstPhaseDen = 10 // 10
 
   val singleIterationOnly: Boolean = false // set freeSkewedThreshold to large number
   val dynamicThreshold: Boolean = false // set freeSkewedThreshold to be equal to threshold
