434b0ef94f1a597c745526461ba6f0edbbd203ba
Avinash Kumar <avinash0161@yahoo.com>
2022-01-12 23:52:01 -0800

    fix issues

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/controller/promisehandlers/MonitoringHandler.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/controller/promisehandlers/MonitoringHandler.scala
@@ -2,10 +2,8 @@ package edu.uci.ics.amber.engine.architecture.controller.promisehandlers
 
 import com.twitter.util.Future
 import edu.uci.ics.amber.engine.architecture.controller.ControllerAsyncRPCHandlerInitializer
-import edu.uci.ics.amber.engine.architecture.controller.promisehandlers.MonitoringHandler.{
-  ControllerInitiateMonitoring,
-  previousCallFinished
-}
+import edu.uci.ics.amber.engine.architecture.controller.promisehandlers.MonitoringHandler.{ControllerInitiateMonitoring, previousCallFinished}
+import edu.uci.ics.amber.engine.architecture.deploysemantics.layer.WorkerWorkloadInfo
 import edu.uci.ics.amber.engine.architecture.worker.promisehandlers.MonitoringHandler.QuerySelfWorkloadMetrics
 import edu.uci.ics.amber.engine.common.rpc.AsyncRPCServer.ControlCommand
 import edu.uci.ics.amber.engine.common.virtualidentity.ActorVirtualIdentity
@@ -40,9 +38,6 @@ trait MonitoringHandler {
             metric.unprocessedDataInputQueueSize + metric.stashedDataInputQueueSize
           workflow.getOperator(worker).getWorkerWorkloadInfo(worker).controlInputWorkload =
             metric.unprocessedControlInputQueueSize + metric.stashedControlInputQueueSize
-          println(
-            s"${worker} - - - - ${workflow.getOperator(worker).getWorkerWorkloadInfo(worker).dataInputWorkload} ---- ${workflow.getOperator(worker).getWorkerWorkloadInfo(worker).controlInputWorkload}"
-          )
         })
       )
 
--- core/amber/src/main/scala/edu/uci/ics/amber/engine/operators/OpExecConfig.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/operators/OpExecConfig.scala
@@ -35,8 +35,12 @@ abstract class OpExecConfig(val id: OperatorIdentity) extends Serializable {
     layer.workers(id)
   }
 
-  def getWorkerWorkloadInfo(id: ActorVirtualIdentity): WorkerWorkloadInfo = {
-    workerToWorkloadInfo.getOrElse(id, WorkerWorkloadInfo(0L, 0L))
+
+  def getWorkerWorkloadInfo(id:ActorVirtualIdentity): WorkerWorkloadInfo = {
+    if(!workerToWorkloadInfo.contains(id)) {
+      workerToWorkloadInfo(id) = WorkerWorkloadInfo(0L,0L)
+    }
+    workerToWorkloadInfo(id)
   }
 
   def setAllWorkerState(state: WorkerState): Unit = {
