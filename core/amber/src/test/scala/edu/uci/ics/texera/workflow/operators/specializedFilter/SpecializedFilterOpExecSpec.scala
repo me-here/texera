5d03b62bf1b0d1c418e89d2cf23f5f7ab7b0b5fa
Mihir Thanekar <mthaneka@uci.edu>
2022-02-04 14:35:43 -0800

    Fixed scalafmt

--- core/amber/src/test/scala/edu/uci/ics/texera/workflow/operators/specializedFilter/SpecializedFilterOpExecSpec.scala
+++ core/amber/src/test/scala/edu/uci/ics/texera/workflow/operators/specializedFilter/SpecializedFilterOpExecSpec.scala
@@ -4,7 +4,12 @@ import edu.uci.ics.amber.engine.common.InputExhausted
 import edu.uci.ics.amber.engine.common.virtualidentity.{LayerIdentity, LinkIdentity}
 import edu.uci.ics.texera.workflow.common.tuple.Tuple
 import edu.uci.ics.texera.workflow.common.tuple.schema.{Attribute, AttributeType, Schema}
-import edu.uci.ics.texera.workflow.operators.filter.{ComparisonType, FilterPredicate, SpecializedFilterOpDesc, SpecializedFilterOpExec}
+import edu.uci.ics.texera.workflow.operators.filter.{
+  ComparisonType,
+  FilterPredicate,
+  SpecializedFilterOpDesc,
+  SpecializedFilterOpExec
+}
 import org.scalatest.BeforeAndAfter
 import org.scalatest.flatspec.AnyFlatSpec
 
@@ -66,7 +71,7 @@ class SpecializedFilterOpExecSpec extends AnyFlatSpec with BeforeAndAfter {
 
   it should "include all tuples from 1 completely null input stream of string type for is null" in {
     val predicates = asList(
-      new FilterPredicate("string", ComparisonType.IS_NULL, null),
+      new FilterPredicate("string", ComparisonType.IS_NULL, null)
     )
     val opExec = new SpecializedFilterOpExec(new SpecializedFilterOpDesc(), predicates)
     opExec.open()
@@ -121,8 +126,6 @@ class SpecializedFilterOpExecSpec extends AnyFlatSpec with BeforeAndAfter {
     opExec.close()
   }
 
-
-
   it should "do nothing when predicates is an empty list" in {
     val opExec = new SpecializedFilterOpExec(new SpecializedFilterOpDesc(), asList())
     opExec.open()
@@ -163,7 +166,7 @@ class SpecializedFilterOpExecSpec extends AnyFlatSpec with BeforeAndAfter {
 
   it should "filter the right values with 2 input streams with is null" in {
     val predicates = asList(
-      new FilterPredicate("bool", ComparisonType.IS_NULL, null),
+      new FilterPredicate("bool", ComparisonType.IS_NULL, null)
     )
     val opExec = new SpecializedFilterOpExec(new SpecializedFilterOpDesc(), predicates)
     opExec.open()
@@ -176,14 +179,14 @@ class SpecializedFilterOpExecSpec extends AnyFlatSpec with BeforeAndAfter {
 
     inclusionRange.map(_ => {
       val result = opExec.processTexeraTuple(Left(allNullTuple()), linkID1)
-      countIncluded = if (result == Iterator()) countIncluded else countIncluded+1
+      countIncluded = if (result == Iterator()) countIncluded else countIncluded + 1
       result
     })
     assert(opExec.processTexeraTuple(Right(InputExhausted()), linkID1).isEmpty)
 
     exclusionRange.map(_ => {
       val result = opExec.processTexeraTuple(Left(nonNullTuple()), linkID2)
-      countIncluded = if (result == Iterator()) countIncluded else countIncluded+1
+      countIncluded = if (result == Iterator()) countIncluded else countIncluded + 1
       result
     })
 
@@ -206,14 +209,14 @@ class SpecializedFilterOpExecSpec extends AnyFlatSpec with BeforeAndAfter {
 
     exclusionRange.map(_ => {
       val result = opExec.processTexeraTuple(Left(allNullTuple()), linkID1)
-      countIncluded = if (result == Iterator()) countIncluded else countIncluded+1
+      countIncluded = if (result == Iterator()) countIncluded else countIncluded + 1
       result
     })
     assert(opExec.processTexeraTuple(Right(InputExhausted()), linkID1).isEmpty)
 
     inclusionRange.map(_ => {
       val result = opExec.processTexeraTuple(Left(nonNullTuple()), linkID2)
-      countIncluded = if (result == Iterator()) countIncluded else countIncluded+1
+      countIncluded = if (result == Iterator()) countIncluded else countIncluded + 1
       result
     })
 
